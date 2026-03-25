5a71b20e32a1d519bcd1b90a7343be4f81120b91
Mantri Yeshwanth <yashmanthri19@gmail.com>
2026-03-25 17:37:51 +0530

    cors issue resolved

--- app.js
+++ app.js
@@ -6,7 +6,11 @@ import taskRoutes from "./routes/taskRoutes.js";
 const app = express();
 
 // Middleware
-app.use(cors());
+app.use(
+  cors({
+    origin: "*",
+  }),
+);
 app.use(express.json());
 
 app.use("/api/auth", authRoutes);
